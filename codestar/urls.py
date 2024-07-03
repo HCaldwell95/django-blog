165f5d4eac61a7ffdb176094be8b563367ab862a
Hayden Caldwell <hade_caldwell@msn.com>
2024-07-03 09:05:47 +0000

    More troubleshooting

--- codestar/urls.py
+++ codestar/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from blog import views as blog_views
+from django.views.generic import RedirectView 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
