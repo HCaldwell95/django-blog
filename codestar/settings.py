418c7d7c5a0326a26b45b8d65c696e7858a502b0
Hayden Caldwell <hade_caldwell@msn.com>
2024-07-24 12:36:39 +0000

    debug set to false

--- codestar/settings.py
+++ codestar/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-hcaldwell95-djangoblog-6b7uegclesb.ws.codeinstitute-ide.net', 
